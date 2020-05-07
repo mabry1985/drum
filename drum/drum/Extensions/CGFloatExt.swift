ddaf37cb829fbf38902f3417812ec9083ab8c380
Josh Mabry <mabry1985@gmail.com>
2020-05-07 13:50:47 -0700

    add random number function

--- drum/drum/Extensions/CGFloatExt.swift
+++ drum/drum/Extensions/CGFloatExt.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2020 Josh Mabry. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
